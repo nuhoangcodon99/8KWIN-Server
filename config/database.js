66f7455ea40daf6aa5cc19d2951e1671002438bd
nuhoangcodon99 <tranvienvien51@gmail.com>
2024-11-16 21:29:02 +0700

    update

---
+++ config/database.js
@@ -0,0 +1,12 @@
+//Mã nguồn được chia sẻ tại youtube : source all
+module.exports = {
+	'url': 'mongodb://127.0.0.1:27017',
+	'options': {
+		//'user':   'game',
+		//'pass':   'game@789',
+		'dbName': '8kwin', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
